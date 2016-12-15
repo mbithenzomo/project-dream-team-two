587490063a2400ea7c8d303b9d88262c4f65f536
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-12-15 21:37:16 +0300

    Add department form

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,1 +1,12 @@
-# Forms for admin blueprint
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class DepartmentForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
