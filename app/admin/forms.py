aa96ac2eb91b2c4ac0d7e4b625302052ec83e4b1
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-12-17 23:17:11 +0300

    Add employee assign form

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired
+
+from ..models import Department, Role
 
 
 class DepartmentForm(FlaskForm):
@@ -18,4 +21,15 @@ class RoleForm(FlaskForm):
     """
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmployeeAssignForm(FlaskForm):
+    """
+    Form for admin to assign departments and roles to employees
+    """
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
     submit = SubmitField('Submit')
